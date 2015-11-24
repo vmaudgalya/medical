d6c3b7694d8649e2d5342e30c293df761c52ea2c
Varun Maudgalya <mvarun321@gmail.com>
2015-11-23 19:06:12 -0800

    Login UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,56 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import mui, { AppBar, Card, FlatButton,
+  TextField, RaisedButton, CircularProgress } from 'material-ui'
+
 
 class App extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      loggedIn: false
+    }
   }
+
+  handleClick() {
+    this.setState({
+      loggedIn: true
+    })
+  }
+
   render() {
+    let view = this.state.loggedIn ?
+    <CircularProgress
+      id="LoginLoader"
+      mode="indeterminate" /> :
+        <div>
+          <AppBar
+            title="Medical App"
+            showMenuIconButton={false} />
+          <Card className="loginCard">
+              <br />
+            <TextField
+              hintText="Username" />
+              <br />
+            <TextField
+              className="passwordField"
+              hintText="Password"
+              type="password" />
+              <br />
+            <RaisedButton
+              label="Login"
+              secondary={true}
+              onClick={this.handleClick.bind(this)}
+              style={{
+                marginTop: '40px',
+                marginLeft: '80px'
+              }} />
+          </Card>
+        </div>
+
     return (
-      <h1>{this.props.message}</h1>      
+      <div className="centered">
+        {view}
+      </div>
     )
   }
 }
